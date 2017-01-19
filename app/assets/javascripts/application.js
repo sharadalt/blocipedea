b817001e66dd1873ada89c32f8787628f6184921
sharadalt <ltsharada@gmail.com>
2017-01-19 23:25:00 +0000

    blocmetrics_test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+var blocmetrics = {};
+  blocmetrics.report = function(eventName){
+    
+    var event = {event: { name: eventName }};
+    
+    var request = new XMLHttpRequest();
+    
+    request.open("POST", "https://blocmetrics-sharadalt.c9users.io/api/events", true);
+    
+    request.setRequestHeader('Content-Type', 'application/json');
+    
+    request.send(JSON.stringify(event));
+    
+  }
